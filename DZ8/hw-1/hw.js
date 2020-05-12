0bbee39b8f8dc058443ffc173028addf316b216a
Long <ya.long2@yandex.ru>
2020-05-13 02:14:01 +0700

    HW8

---
+++ DZ8/hw-1/hw.js
@@ -0,0 +1,11 @@
+// Создать две переменные: name, admin
+// В переменную name запишите своё имя
+// В переменную admin запишите значение переменной name
+// выведите на экран admin, через функцию alert
+
+'use strict'
+
+let name, admin;
+name = 'Long';
+admin = name;
+alert(admin);
