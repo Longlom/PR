b75842a5b624461d2456ddeb16d673e34fbfcbde
Long <ya.long2@yandex.ru>
2020-05-21 05:14:49 +0700

    HW10

---
+++ DZ10/script.js
@@ -0,0 +1,57 @@
+// Домашнее задание:
+// Задание 1: Функция, возвращающая объект, и меняющая местами ключ <-> значение.
+// исходный объект должен остаться неизменным.
+// Уровень 1: Работа с конкретныйм объектом и конкретными его свойствами
+
+const firstObject = {
+  'one': 'number',
+};
+
+function resolve1(inputObject) {
+  let obj = {};
+  obj[inputObject['one']] = 'one';
+  return obj;
+};
+
+const result1 = resolve1(firstObject);
+console.log(result1); // ожидаемый результат { 'number': 'one' }
+console.log(firstObject); // ожидаемый результат { 'one': 'number' }
+
+
+// Уровень 2: Работа с любым объектом БЕЗ вложенных объектов внутри
+// и любым количеством свойствам
+// Данный объект. Должен остаться неизменным
+const secondObject = {
+  'apple': 'fruit',
+  'potato': 'vegetable',
+  'strawberry': 'berry',
+};
+
+function resolve2(inputObject) {
+  let obj = {};
+  for (let key in inputObject) {
+    obj[inputObject[key]] = key;
+  }
+  return obj;
+};
+
+const result2 = resolve2(secondObject);
+console.log(result2);
+// ожидаемый результат: { 'apple': 'fruit', 'potato': 'vegetable', 'strawberry': 'berry' }
+
+console.log(secondObject);
+// ожидаемый результат { 'fruit': 'apple', 'vegetable': 'potato', 'berry': 'strawberry' }
+
+
+// Задание 2. Написать функцию, возвращающую век, соответствующий данному году
+// Использовать Глобальный объект Math
+function centuryFromYear(year) {
+  return Math.ceil(year / 100);
+}
+
+
+const year = 1905;
+console.log(centuryFromYear(year)); // 20
+
+const year2 = 1700;
+console.log(centuryFromYear(year2)); // 17.
